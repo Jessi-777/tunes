761f54e74cb77452e57a525a63cfe484d57d3f96
Jessi Chavez Gallego <angelwerkengineeringmail.com>
2023-07-16 23:47:56 -0700

    added music player test2

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    // reactStrictMode: true,
-    // optimizeFonts: false,
+    reactStrictMode: true,
+    optimizeFonts: false,
 }
 
 module.exports = nextConfig;
